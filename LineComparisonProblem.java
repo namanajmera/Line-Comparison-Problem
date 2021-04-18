d66ede477052fc17f7f0a87674fd30321ee8f585
Naman <cool.naman.ajmera@gmail.com>
2021-04-18 16:53:36 +0530

    Modified: Compare two lines using compare to method

--- LineComparisonProblem.java
+++ LineComparisonProblem.java
@@ -1,45 +1,47 @@
 import java.util.Scanner;
 
 public class LineComparisonProblem {
+    public static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the First lines co-ordinates.");
-        System.out.println("Enter the Co-ordinates of Line First - First Point:- ");
-        int x1 = sc.nextInt();
-        int y1 = sc.nextInt();
+        // Line 1
+        double length1 = Line1();
 
-        System.out.println("Enter the Co-ordinates of  Line First - Second Point:- ");
-        int x2 = sc.nextInt();
-        int y2 = sc.nextInt();
-
-        // Finding the lenght of First Line
-        double length1 = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-        System.out.println("Length of line 1 is:- " + length1);
-
-        System.out.println("Enter the Second lines co-ordinates.");
-        System.out.println("Enter the Co-ordinates of Line Second - First Point:- ");
-        int x3 = sc.nextInt();
-        int y3 = sc.nextInt();
-
-        System.out.println("Enter the Co-ordinates of Line Second - Second Point:- ");
-        int x4 = sc.nextInt();
-        int y4 = sc.nextInt();
-
-        // Finding the lenght of Second Line
-        double length2 = Math.sqrt(Math.pow((x4 - x3), 2) + Math.pow((y4 - y3), 2));
-
-        System.out.println("Length of line 2 is:- " + length2);
-
-        // Finding if lenght is equal or not:-
-        // First we have to create the object of Double by putting length in it.
+        // Line 2
+        double length2 = Line2();
 
         Double len1 = new Double(length1);
         Double len2 = new Double(length2);
 
-        if (len1.equals(len2)) {
-            System.out.println("Both lines are equals.");
-        } else {
-            System.out.println("Lines are not equals.");
-        }
+        System.out.println("Java compareTo method result = " + len1.compareTo(len2));
+        System.out.println("Java equals method result = " + len1.equals(len2));
+
+    }
+
+    public static double Line1() {
+        System.out.println("First line Configuration");
+        System.out.println("Enter the co-ordinates of First Point x1 y1");
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+
+        System.out.println("Enter the co-ordinates of Second Point x2 y2");
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
+
+        return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+    }
+
+    public static double Line2() {
+        System.out.println("Second line Configuration");
+        System.out.println("Enter the co-ordinates of First Point x3 y3");
+        int x3 = sc.nextInt();
+        int y3 = sc.nextInt();
+
+        System.out.println("Enter the co-ordinates of Second Point x4 y4");
+        int x4 = sc.nextInt();
+        int y4 = sc.nextInt();
+
+        return Math.sqrt(Math.pow((x4 - x3), 2) + Math.pow((y4 - y3), 2));
+
     }
 }
